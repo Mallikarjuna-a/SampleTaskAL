71993c2bb335bed6b26390f8447d356bb0948a79
Mallikarjuna <adramallikarjuna095@gmail.com>
2021-01-18 22:43:57 +0530

    Initial Commit

---
+++ SampleTaskAL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleTaskAL
+//
+//  Created by Mallikarjuna on 18/01/21.
+//  Copyright © 2021 Mallikarjuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
